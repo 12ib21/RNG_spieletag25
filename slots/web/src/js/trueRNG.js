041fa13519e4feb297a4be5c7e32af78055390ec
12ib21 <iren.biggel@gmail.com>
2025-07-06 13:03:38 +0200

    rng fix

--- slots/web/src/js/trueRNG.js
+++ slots/web/src/js/trueRNG.js
@@ -1,7 +1,7 @@
 function getTrueRandomInt() {
     const randomBuffer = new Uint32Array(1);
     window.crypto.getRandomValues(randomBuffer);
-    return randomBuffer[0];
+    return randomBuffer[0] / ((Math.pow(2, 32)) - 1);
 }
 
 function generateTrueRandomValues(count) {

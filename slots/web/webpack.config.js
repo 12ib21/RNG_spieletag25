6163919028394f5df4904063a62ded640b4c5b00
Iren Biggel <iren.biggel@gmail.com>
2025-11-18 13:41:44 +0100

    diverse bugfixes

--- slots/web/webpack.config.js
+++ slots/web/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: ["./src/js/index.js", "./src/css/style.css", "./src/css/bootstrap.min.css"],
@@ -23,6 +24,12 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             favicon: "./src/assets/favicon.ico",
+        }),
+        // Copy only the logo image into the dist root (no `assets/` subfolder)
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, "src/assets/logo.png"), to: "." },
+            ],
         }),
         new MiniCssExtractPlugin({
             filename: "main.[contenthash].css",
